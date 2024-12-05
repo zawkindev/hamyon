97990863427e584f5ea339d24181f3060ac62c00
zawkindev <zawkindev@gmail.com>
2024-12-05 13:39:58 +0500

    fix: add properties for postgresql

--- src/main/java/zawkin/me/kawori/HamyonApplication.java
+++ src/main/java/zawkin/me/kawori/HamyonApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HamyonApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HamyonApplication.class, args);
 	}
-
 }
