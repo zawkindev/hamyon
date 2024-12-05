e9d86f59e9ffbc182969b7fce5eb55f6c8a1512a
zawkindev <zawkindev@gmail.com>
2024-12-05 09:18:51 +0500

    initialize spring

---
+++ src/main/java/zawkin/me/kawori/HamyonApplication.java
@@ -0,0 +1,13 @@
+package zawkin.me.kawori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HamyonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HamyonApplication.class, args);
+	}
+
+}
